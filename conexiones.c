6a2f09a04a24a37e333e3e143675b970fd6343fe
sergiodiaz130 <sergio.diazgarcia@alum.uca.es>
2026-03-19 16:48:08 +0100

    Agregar implementación de funciones para manejar conexiones y liberar recursos

--- conexiones.c
+++ conexiones.c
@@ -3,3 +3,57 @@
 #include <string.h>
 #include "conexiones.h"
 
+Conexion* cargar_conexiones(const char* nombre_fichero, int* num_conexiones) {
+}
+
+void listar_salidas_disponibles(Conexion* conexiones, int num_conexiones, int id_sala) {
+    int encontradas = 0;
+    printf("\n--- Salidas desde la sala %02d ---\n", id_sala);
+    
+    for (int i = 0; i < num_conexiones; i++) {
+        if (conexiones[i].id_origen == id_sala) {
+            printf("- Hacia sala %02d (Estado: %s)\n", conexiones[i].id_destino, conexiones[i].estado);
+            encontradas = 1;
+        }
+    }
+    
+    if (encontradas == 0) {
+        printf("No hay salidas desde esta sala.\n");
+    }
+}
+
+int verificar_conexion(Conexion* conexiones, int num_conexiones, int id_origen, int id_destino) {
+    int existe = 0;
+    
+    for (int i = 0; i < num_conexiones && existe == 0; i++) {
+        if (conexiones[i].id_origen == id_origen && conexiones[i].id_destino == id_destino) {
+            existe = 1;
+        }
+    }
+    
+    return existe;
+}
+
+int comprobar_estado_conexion(Conexion* conexiones, int num_conexiones, int id_origen, int id_destino) {
+    int estado_activo = 0;
+    
+    for (int i = 0; i < num_conexiones && estado_activo == 0; i++) {
+        if (conexiones[i].id_origen == id_origen && conexiones[i].id_destino == id_destino) {
+            if (strcmp(conexiones[i].estado, "Activa") == 0) {
+                estado_activo = 1;
+            }
+        }
+    }
+    
+    return estado_activo;
+}
+
+int desbloquear_conexion(Conexion* conexiones, int num_conexiones, int id_origen, int id_destino, const char* condicion_aportada) {
+    
+}
+
+void liberar_conexiones(Conexion* conexiones) {
+    if (conexiones != NULL) {
+        free(conexiones);
+    }
+}
--- conexiones.h
+++ conexiones.h
@@ -2,13 +2,19 @@
 #define CONEXIONES_H
 
 typedef struct {
-    char id_conexion[3];
+    char id_conexion[4];
     int id_origen;
     int id_destino;
     char estado[15];
     char cond[10];
 } Conexion;
 
+Conexion* cargar_conexiones(const char* nombre_fichero, int* num_conexiones);
+void listar_salidas_disponibles(Conexion* conexiones, int num_conexiones, int id_sala);
+int verificar_conexion(Conexion* conexiones, int num_conexiones, int id_origen, int id_destino);
+int comprobar_estado_conexion(Conexion* conexiones, int num_conexiones, int id_origen, int id_destino);
+int desbloquear_conexion(Conexion* conexiones, int num_conexiones, int id_origen, int id_destino, const char* condicion_aportada);
+void liberar_conexiones(Conexion* conexiones);
 
 
 #endif
